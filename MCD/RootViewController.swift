bece8bea780b0f5e640ffc8c61118489faff934c
Weiyi Kong <git@momok.cyou>
2022-08-15 22:49:52 +1000

    setup project

---
+++ MCD/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  MCD
+//
+//  Created by Weiyi Kong on 15/8/2022.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
